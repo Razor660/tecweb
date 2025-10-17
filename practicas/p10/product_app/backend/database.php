97d20c6c11a4757e317e0c99766982c1810f16af
Razor660 <link660pro@gmail.com>
2025-10-16 23:43:01 -0600

    modificada conexion a bdd

--- practicas/p10/product_app/backend/database.php
+++ practicas/p10/product_app/backend/database.php
@@ -1,15 +1,16 @@
 <?php
-    $conexion = @mysqli_connect(
-        'localhost',
-        'root',
-        '@Nothing30',
-        'marketzone'
-    );
 
-    /**
-     * NOTA: si la conexión falló $conexion contendrá false
-     **/
-    if(!$conexion) {
-        die('¡Base de datos NO conextada!');
-    }
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '@Nothing30'); 
+define('DB_NAME', 'marketzone');
+
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$conn) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
 ?>
