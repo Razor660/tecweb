e3afe746c4db2b43a77685fc0d478aeb0903408d
Razor660 <link660pro@gmail.com>
2025-10-24 15:05:45 -0600

    conexión con UTF-8 asegurada

--- practicas/p11_con_jquery/product_app/backend/database.php
+++ practicas/p11_con_jquery/product_app/backend/database.php
@@ -1,8 +1,8 @@
 <?php
     $conexion = @mysqli_connect(
         'localhost',
-        'root',
-        '@Nothing30',
+        'root',  
+        '@Nothing30', 
         'marketzone'
     );
 
@@ -10,6 +10,9 @@
      * NOTA: si la conexión falló $conexion contendrá false
      **/
     if(!$conexion) {
-        die('¡Base de datos NO conextada!');
+        die('¡Base de datos NO conectada!');
     }
+    
+    // Aseguramos que la conexión maneje UTF-8
+    $conexion->set_charset("utf8");
 ?>
