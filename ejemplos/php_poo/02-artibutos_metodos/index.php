145b0f84511b0d0815e0bdd1f3321353965caa62
Razor660 <link660pro@gmail.com>
2025-11-05 13:51:08 -0600

    carpeta de ejemplos de POO en PHP

---
+++ ejemplos/php_poo/02-artibutos_metodos/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>02-Atribbutos y Métodos</title>
+</head>
+<body>
+    <?php
+    require_once __DIR__.'/Menu.php';
+    $menu1 = new Menu();
+    $menu1->cargar_opcion('https://facebook.com', 'Facebook');
+    $menu1->cargar_opcion('https://instagram.com', 'Instagram');
+    $menu1->cargar_opcion('https://x.com', 'X');
+
+    $menu1->graficar();
+
+
+
+    ?>
+
+</body>
+</html>
---
+++ ejemplos/php_poo/03-constructor_namespace/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>03-Constructor y Namespace</title>
+</head>
+<body>
+    <?php
+    use ejemplos\php_poo\Cabecera2 as Cabecera;
+    require_once __DIR__.'/Cabecera.php';
+
+    $cab1 = new Cabecera('El rincón del Programador', 'center', 'https://www.cs.buap.mx');
+    $cab1->graficar();
+
+    ?>    
+
+
+</body>
+</html>
