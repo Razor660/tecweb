8485efdf3c7841e7fce4c2568c2adb3566ca08b9
Razor660 <link660pro@gmail.com>
2025-11-20 22:57:15 -0600

    Ahora usa composer

--- practicas/p12-product_app-con_clases/product_app/backend/product-add.php
+++ practicas/p12-product_app-con_clases/product_app/backend/product-add.php
@@ -1,8 +1,8 @@
 <?php
-    use TECWEB\MYAPI\Products;
-    require_once __DIR__.'/myapi/Products.php';
+    use TECWEB\MYAPI\Create\Create; // Usar la nueva clase
+    require_once __DIR__.'/../vendor/autoload.php'; // Usar el autoloader
 
-    $productos = new Products('marketzone');
+    $productos = new Create('marketzone'); // Instanciar la nueva clase
     $productos->add( json_decode( json_encode($_POST) ) );
     echo $productos->getData();
 ?>
