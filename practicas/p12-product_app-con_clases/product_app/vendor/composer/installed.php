8ce7269bd7f5d1ddb8e0eb0c88f27194239eb634
Razor660 <link660pro@gmail.com>
2025-11-20 22:50:04 -0600

    Se instaló composer al proyecto

---
+++ practicas/p12-product_app-con_clases/product_app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '17fb85bb4506c31d756e613cd6cb34da7df47a92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '17fb85bb4506c31d756e613cd6cb34da7df47a92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
